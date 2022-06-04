2f41feebb3b15004b02e3258c60e4f0d692b8987
Sacha FARINEL <farinel.sacha@tutanota.com>
2022-06-05 00:50:20 +0200

    delete + route with delete methods + token + flashes

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -31,10 +31,36 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @param Property $property
+     * @param Request $request
+     * @param ManagerRegistry $doctrine
      * @return Response
      */
-    #[Route('/admin/{id}', name: 'admin.property.edit')]
+    #[Route('/admin/property/create', name: 'admin.property.new')]
+    public function new(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($property);
+            $entityManager->flush();
+            $this->addFlash('success', 'Bien crée avec succès');
+            return $this->redirectToRoute('admin.property.index');
+        }
+        return $this->render('admin/new.html.twig', [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @param Property $property
+     * @param Request $request
+     * @param ManagerRegistry $doctrine
+     * @return Response
+     */
+    #[Route('/admin/property/{id}', name: 'admin.property.edit', methods: "GET|POST")]
     public function edit(Property $property, Request $request, ManagerRegistry $doctrine): Response
     {
         $form = $this->createForm(PropertyType::class, $property);
@@ -42,12 +68,29 @@ class AdminController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $doctrine->getManager();
             $entityManager->flush();
+            $this->addFlash('success', 'Bien modifié avec succès');
             return $this->redirectToRoute('admin.property.index');
         }
         return $this->render('admin/edit.html.twig', [
             'property' => $property,
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @param Property $property
+     * @param ManagerRegistry $doctrine
+     */
+    #[Route('/admin/property/{id}', name: 'admin.property.delete', methods: "DELETE")]
+    public function delete(Property $property, ManagerRegistry $doctrine, Request $request): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))) {
+            $entityManager = $doctrine->getManager();
+            $entityManager->remove($property);
+            $entityManager->flush();
+            $this->addFlash('success', 'Bien supprimé avec succès');
+        }
+        return $this->redirectToRoute('admin.property.index');
     }
 
 }
