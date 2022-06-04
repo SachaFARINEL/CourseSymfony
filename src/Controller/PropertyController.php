b2cc9865c759b4da4dad0623feeb0fa9f0017944
Sacha FARINEL <farinel.sacha@tutanota.com>
2022-06-04 11:15:16 +0200

    First commit

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    #[Route('/biens', name: 'property.index')]
+    public function index(): Response
+    {
+        return new Response("Les biens");
+    }
+}
